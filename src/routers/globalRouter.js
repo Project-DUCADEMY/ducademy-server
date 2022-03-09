d069ca8907a46340525e852eb07cc77a160c5d02
건호 김 <qfourca3305@gmail.com>
2022-03-09 18:34:21 +0900

    Logout router

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { join, login } from "../controllers/userControllers"
+import { join, login, logout } from "../controllers/userControllers"
 
 const globalRouter = express.Router()
 
 globalRouter.post("/join", join)
 
 globalRouter.post("/login", login)
+
+globalRouter.post("/logout", logout)
 
 export default globalRouter
