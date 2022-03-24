63b174ec19bb9fa238232724cc1bc6b5b87ea72d
tomato <tomato6726@gmail.com>
2022-03-24 20:27:39 +0900

    .

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose"
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: false },
-  job: { type: String, required: true },
+  name: { type: String, required: false },
+  address: { type: String, required: false },
+  phoneNumber: { type: String, required: false },
+  birthDay: { type: Number, required: false },
 })
 
 const User = mongoose.model("User", userSchema)
